c5bd092b1ec7c1de7210a8ce4ec23cc443b2d1b9
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2025-10-16 00:51:44 +0100

    I moved around 2 packages and got d3 to work, added in json data for the map of scotland. In the middle of making a map using the data with d3

--- Prototype/server.js
+++ Prototype/server.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var app = express();
+// var d3 = require('d3');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.use(express.static('Public'));
+
+// app.use(express.static('client_js'));
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index.ejs');
 });
 
 // login page
 app.get('/login', function(req, res) {
-  res.render('pages/login');
+  res.render('pages/login.ejs');
 });
 
 // map page
 app.get('/map', function(req, res) {
-  res.render('pages/map');
+  res.render('pages/map.ejs');
 });
+
+app.get('/test', function(req, res) {
+  res.render('pages/test.ejs');
+});
+
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
